46fb3cb8618d105ecabd63d3b970d04334191cc4
Volcan'Tech <lardinoisstevenfl@gmail.com>
2025-11-27 01:08:49 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,19 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   build: {
-    outDir: "dist/spa",
+    outDir: 'dist/spa',
   },
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client"),
-      "@shared": path.resolve(__dirname, "./shared"),
+      '@': path.resolve(__dirname, './client'),
+      '@shared': path.resolve(__dirname, './shared'),
     },
   },
 });
