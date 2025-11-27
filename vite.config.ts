62ca7c313f2c34a8f23229b7a3ea4ef7d13b68fd
Volcan'Tech <lardinoisstevenfl@gmail.com>
2025-11-27 01:06:09 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: "0.0.0.0",
-    port: 5000,
-    strictPort: true,
-    allowedHosts: true,
-    hmr: false,
-    fs: {
-      allow: [".", "./client", "./shared"],
-      deny: [".env", ".env.*", "*.{crt,pem}", "**/.git/**", "server/**"],
-    },
-  },
   build: {
     outDir: "dist/spa",
   },
