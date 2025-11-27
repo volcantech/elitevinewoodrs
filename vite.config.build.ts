55a3d5d6bf72312f2660195af48a242cacf53465
Volcan'Tech <lardinoisstevenfl@gmail.com>
2025-11-27 01:01:01 +0100

    Create vite.config.build.ts

---
+++ vite.config.build.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+
+export default defineConfig({
+  build: {
+    outDir: "dist/spa",
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./client"),
+      "@shared": path.resolve(__dirname, "./shared"),
+    },
+  },
+});
