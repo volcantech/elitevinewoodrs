63fddbbb90995d02c8a54a8815d1408142e39bef
Volcan'Tech <lardinoisstevenfl@gmail.com>
2025-12-07 02:10:53 +0100

    Delete client directory

--- client/types/permissions.ts
+++
@@ -1,7 +0,0 @@
-export interface UserPermissions {
-  vehicles: { view: boolean; create: boolean; update: boolean; delete: boolean };
-  orders: { view: boolean; validate: boolean; cancel: boolean; delete: boolean };
-  users: { view: boolean; create: boolean; update: boolean; delete: boolean };
-  moderation: { view: boolean; ban_uniqueids: boolean; view_logs: boolean };
-  announcements: { view: boolean; create: boolean; update: boolean; delete: boolean };
-}
