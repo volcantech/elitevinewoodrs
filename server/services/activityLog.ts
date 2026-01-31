cacb719bf89b07ba53dac579e16ad6bba202f931
Volcan'Tech <lardinoisstevenfl@gmail.com>
2026-01-31 15:46:02 +0100

    Update activityLog.ts

--- server/services/activityLog.ts
+++ server/services/activityLog.ts
@@ -1,6 +1,6 @@
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(process.env.EXTERNAL_DATABASE_URL!);
+const sql = neon(process.env.NETLIFY_DATABASE_URL!);
 
 export async function initActivityLogsTable() {
   try {
