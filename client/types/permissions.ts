c0e66c57a8f60b7079b5925f8c977c86d17fad89
Volcan'Tech <lardinoisstevenfl@gmail.com>
2026-03-27 00:22:23 +0100

    Add files via upload

---
+++ client/types/permissions.ts
@@ -0,0 +1,10 @@
+export interface UserPermissions {
+  vehicles: { view: boolean; create: boolean; update: boolean; delete: boolean; toggle_categories: boolean };
+  orders: { view: boolean; validate: boolean; cancel: boolean; delete: boolean };
+  users: { view: boolean; update: boolean; manage_admin: boolean };
+  moderation: { view: boolean; ban_uniqueids: boolean; view_logs: boolean; ban_players: boolean };
+  announcements: { view: boolean; create: boolean; update: boolean; delete: boolean };
+  reviews: { view: boolean; delete: boolean };
+  loyalty: { manage: boolean };
+  webhooks: { manage: boolean };
+}
