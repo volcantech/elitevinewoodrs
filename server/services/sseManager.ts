20fc41fa7f698b6297239112a76fd037690c3de7
Volcan'Tech <lardinoisstevenfl@gmail.com>
2026-05-08 21:12:20 +0200

    Add files via upload

---
+++ server/services/sseManager.ts
@@ -0,0 +1,27 @@
+import type { Response } from "express";
+
+const sseClients = new Map<string, Set<Response>>();
+
+export function addSseClient(token: string, res: Response) {
+  if (!sseClients.has(token)) sseClients.set(token, new Set());
+  sseClients.get(token)!.add(res);
+}
+
+export function removeSseClient(token: string, res: Response) {
+  const set = sseClients.get(token);
+  if (set) {
+    set.delete(res);
+    if (set.size === 0) sseClients.delete(token);
+  }
+}
+
+export function broadcastSse(token: string, event: string, data: unknown) {
+  const clients = sseClients.get(token);
+  if (!clients || clients.size === 0) return;
+  const msg = `event: ${event}\ndata: ${JSON.stringify(data)}\n\n`;
+  for (const res of clients) {
+    try {
+      res.write(msg);
+    } catch {}
+  }
+}
